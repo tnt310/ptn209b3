b59744dc461cc13ad6c5e24fbfd3a5960a4b071c
Tram Tran <trantram2299@gmail.com>
2021-04-13 23:49:38 +0700

    add

--- time_value.c
+++ time_value.c
@@ -2,11 +2,13 @@
 #include <string.h>
 
 int channel = 1;
-static int i = 3;
+static int i = 0;
 char reg[10];
 char dev[10];
 char publish_buffer[1000];
-
+char chan[5] ="CH1";
+char buffer[50];
+char time[20] = "10:10:25 26.6.2021";
 /*------------------------------------------	------------------------------------*/
 typedef struct{ // STRUCT DATA FROM EEPROM
     int id; // id
@@ -18,12 +20,12 @@ typedef struct{ // STRUCT DATA FROM EEPROM
 /*------------------------------------------------------------------------------*/
 data_t table[200] =
 {
-	1,	3,	5,	"Temperture", "INV",
-	10, 3,  4,	"Humidity", "SENSOR",
-	1,	3,	2,	"Lumen", "SENSOR",
-	2,	3,	8,	"Current", "INV",
-	2,	3,	9,	"Voltage", "INV",
-	5, 3, 	5,	"Power", "INV"
+	1,	3,	5,	"Temperture", "INV1",
+	10, 3,  4,	"Humidity", "SENSOR2",
+	1,	3,	2,	"Lumen", "SENSOR4",
+	2,	3,	8,	"Current", "INV2",
+	2,	3,	9,	"Voltage", "INV2",
+	5, 3, 	5,	"Power", "INV5"
 };
 /*----------------------------ITOA_USER--------------------------------------*/
 int* itoa_user(int val, int  base) {
@@ -38,7 +40,7 @@ int* itoa_user(int val, int  base) {
 	return &buf[i + 1];
 }
 /*----------------------------GET REG NAME AND DEV NAME--------------------------------------*/
-int Get_name(char name_reg[10],char name_dev[10],int channel, unsigned int id, unsigned int func, unsigned short reg)
+int Get_name(char name_reg[10],char name_dev[10],char channel[5], unsigned int id, unsigned int func, unsigned short reg)
 {
 	memset(name_reg,0,10);
     memset(name_dev,0,10);
@@ -49,21 +51,42 @@ int Get_name(char name_reg[10],char name_dev[10],int channel, unsigned int id, u
 	}
 }
 /*----------------------------CREATE JSON STRING--------------------------------------------------*/
-int createJson(char demo[50],int channel,char name_reg[10],char name_dev[10], int val)
+int createJson(char demo[50],char channel[5],char name_reg[10],char name_dev[10], int val)
 {
 	memset(demo,0,50);
-	strcat(demo,"\"");
+	//strcat(demo,"{\"");
+    strcat(demo,"{");
+	//strcat(demo,"\"");
+	strcat(demo,chan);
+	strcat(demo,"_");
 	strcat(demo,name_dev);
-	strcat(demo,"\"");
+	strcat(demo,"_");
+	strcat(demo,name_reg);
+	//strcat(demo,"\"");
 	strcat(demo,":");
-	strcat(demo,itoa_user(val, 10));
+	//strcat(demo,itoa_user(val, 10));
+    strcat(demo,"[{time:");
+    strcat(demo,time);
+    strcat(demo,",");
+    strcat(demo,"value");
+    strcat(demo,":");
+    strcat(demo,itoa_user(val, 10));
+    strcat(demo,"}]");
+    strcat(demo,"}");
+
 	}
 /*------------------------------------------------------------------------------*/
 int main()
 {
 
-    Get_name(reg, dev, channel, table[i].id, table[i].func, table[i].reg_adr);
-    printf("%s\n",reg);
-    printf("%s",dev);
+    //Get_name(reg, dev, chan, table[i].id, table[i].func, table[i].reg_adr);
+    //printf("%s\n",reg);
+   // printf("%s",dev);
+   for (i = 0; i< 5; i++)
+   {
+        createJson(buffer,chan,table[i].name,table[i].name_dev,table[i].reg_adr);
+        printf("\n%s",buffer);
+   }
     return 0;
 }
+// CH1_INV1_VOLT: 	[{time: 10:10:25 26.6.2021, value: 220}],
