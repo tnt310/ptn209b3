7491af48c4342e2ea5c9965db424e39c91d7d033
Tram Tran <trantram2299@gmail.com>
2021-04-13 23:32:38 +0700

    add

---
+++ time_value.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+
+int channel = 1;
+static int i = 3;
+char reg[10];
+char dev[10];
+char publish_buffer[1000];
+
+/*------------------------------------------	------------------------------------*/
+typedef struct{ // STRUCT DATA FROM EEPROM
+    int id; // id
+    int func;
+    unsigned short reg_adr; // Adress reg
+    char name[10]; // name of address reg
+    char name_dev[10];
+}data_t;
+/*------------------------------------------------------------------------------*/
+data_t table[200] =
+{
+	1,	3,	5,	"Temperture", "INV",
+	10, 3,  4,	"Humidity", "SENSOR",
+	1,	3,	2,	"Lumen", "SENSOR",
+	2,	3,	8,	"Current", "INV",
+	2,	3,	9,	"Voltage", "INV",
+	5, 3, 	5,	"Power", "INV"
+};
+/*----------------------------ITOA_USER--------------------------------------*/
+int* itoa_user(int val, int  base) {
+
+	static int buf[32] = { 0 };  // 32 bits
+	int i = 30;
+	if (val == 0)
+		buf[i--] = '0';
+	for (; val && i; --i, val /= base)
+		buf[i] = "0123456789abcdef"[val % base];
+
+	return &buf[i + 1];
+}
+/*----------------------------GET REG NAME AND DEV NAME--------------------------------------*/
+int Get_name(char name_reg[10],char name_dev[10],int channel, unsigned int id, unsigned int func, unsigned short reg)
+{
+	memset(name_reg,0,10);
+    memset(name_dev,0,10);
+	for ( int j = 0; j< 10; j++)
+	{
+		name_reg[j] = table[i].name[j];
+        name_dev[j] = table[i].name_dev[j];
+	}
+}
+/*----------------------------CREATE JSON STRING--------------------------------------------------*/
+int createJson(char demo[50],int channel,char name_reg[10],char name_dev[10], int val)
+{
+	memset(demo,0,50);
+	strcat(demo,"\"");
+	strcat(demo,name_dev);
+	strcat(demo,"\"");
+	strcat(demo,":");
+	strcat(demo,itoa_user(val, 10));
+	}
+/*------------------------------------------------------------------------------*/
+int main()
+{
+
+    Get_name(reg, dev, channel, table[i].id, table[i].func, table[i].reg_adr);
+    printf("%s\n",reg);
+    printf("%s",dev);
+    return 0;
+}
