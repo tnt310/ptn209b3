d5d8f6b2c37b94175714546bca842bbdec91a5f9
Tram Tran <trantram2299@gmail.com>
2021-04-22 02:23:48 +0700

    add

--- test.c
+++ test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <stdlib.h>
 typedef struct{ // STRUCT DATA FROM EEPROM
     int id; // id
     int func;
@@ -23,9 +24,20 @@ extern data_t table[] =   // CHANNEL 1
 		3,	3,	9,	"POWR",		"METER1"
 
 };
+char aux_str[100];
+char server[]="tcp://m14.cloumqtt.com";
+int port = 19613;
+int keepalive = 180;
+char username[]="yktdxpqb";
+char password[]="VKCG6yboYrYd";
+int state = 0;
 
 int main()
 {
-    int a =  ((sizeof(table))/(sizeof(data_t)));
-    printf("%d",a);
+    // int a =  ((sizeof(table))/(sizeof(data_t)));
+    // printf("%d",a);
+	snprintf(aux_str, sizeof(aux_str),"AT+CMQTTCONNECT=%d,\"%s:%d\",%d,%d,\"%s\",\"%s\"",state,server,port,keepalive,1,username,password);
+	printf("%s",aux_str);
 }
+//	AT+CMQTTCONNECT=0,"tcp://m14.cloudmqtt.com:19613",180,1,"yktdxpqb","VKCG6yboYrYd"<CR><LF>
+//  sprintf(api,"%s%c",GET,(char) 26);
