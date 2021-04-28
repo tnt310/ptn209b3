481ce5bc0cbecd62cfddd5001d1171a3043749d2
Tram Tran <trantram2299@gmail.com>
2021-04-29 02:40:22 +0700

    add

--- Thesis/copy_table.c
+++ Thesis/copy_table.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdint.h>
 
-typedef struct{ // STRUCT DATA FROM EEPROM
+typedef struct{ 
 	uint8_t channel;
     uint8_t id; // id
     uint8_t func; // function code
@@ -11,20 +11,15 @@ typedef struct{ // STRUCT DATA FROM EEPROM
     char *title; // name of address reg
     char *type; // number or boolean
 }data1_t;
-data1_t table1[] = 
-{
-		0,	2,	3,	0,   "SENSOR",		"TEMPERATURE",	"NUMBER",
-		0,	2, 	3,  1, 	 "SENSOR",		"HUMIDITY",	    "NUMBER",
-		0,	2,	3,	2,	 "INVERTER",	"POWER",	    "NUMBER",
-		0,	2,	3,	3,	 "SENSOR",		"LUMEN",	    "NUMBER",
-		0,	2,	3,	4,	 "METER",	 	"CURRENT",	    "NUMBER",
-		0,	2,	3,	5,	 "METER",		"VOLTAGE",	    "NUMBER",
-		0,	2,  3, 	6,	 "INVERTER",	"POWER",	    "NUMBER",
-        1,  3,  6,  10,   "RELAY",       "LIGHT",       "BOOLEAN",
-        1,  3,  6,  15,   "RELAY",       "LIGHT",       "BOOLEAN",
-        2,  4,  6,  11,   "RELAY",       "FAN",       "BOOLEAN",
-       
-};
+
+typedef struct{
+	uint8_t channel;
+    uint8_t id;
+    uint8_t func;
+    uint16_t reg_adr;
+    char *alias;
+}data_t;
+
 uint8_t  createJson_provision(char *buffer, char *name,uint8_t channel, uint8_t id, char *type, char *title, char *description)
 {
         memset(buffer,0,sizeof(buffer));
@@ -102,15 +97,52 @@ uint8_t create_name(char buffer[20],uint8_t channel,uint8_t id,uint16_t reg, cha
          }
     }
 }
+data1_t table1[] = 
+{
+		0,	2,	3,	0,   "SENSOR",		"TEMPERATURE",	"NUMBER",
+		0,	2, 	3,  1, 	 "SENSOR",		"HUMIDITY",	    "NUMBER",
+		0,	2,	3,	2,	 "INVERTER",	"POWER",	    "NUMBER",
+		0,	2,	3,	3,	 "SENSOR",		"LUMEN",	    "NUMBER",
+		0,	2,	3,	4,	 "METER",	 	"CURRENT",	    "NUMBER",
+		0,	2,	3,	5,	 "METER",		"VOLTAGE",	    "NUMBER",
+		0,	2,  3, 	6,	 "INVERTER",	"POWER",	    "NUMBER",
+        1,  3,  6,  10,   "RELAY",       "LIGHT",       "BOOLEAN",
+        1,  3,  6,  15,   "RELAY",       "LIGHT",       "BOOLEAN",
+        2,  4,  6,  11,   "RELAY",       "FAN",         "BOOLEAN",
+};
+data_t table[];
+
+uint8_t convert_table(data_t t1[], data1_t t2[])
+{
+    char name[20];
+    uint8_t sum = sizeof(t2)/sizeof(data1_t);
+    printf("%d",sum);
+    for (uint8_t i = 0 ; i < sum ; i++)
+    {
+        t1[i].channel = t2[i].channel;
+        t1[i].id = t2[i].id;
+        t1[i].func = t2[i].func;
+        t1[i].reg_adr = t2[i].reg_adr;
+        create_name(name,t2[i].channel, t2[i].id,t2[i].reg_adr, t2[i].title, t2[i].description);
+        t1[i].alias = name;
+        printf("%d\t%d\t%d\t%d\t%s\n",t1[i].channel, t1[i].id,t1[i].func,t1[i].reg_adr, t1[i].alias);
+    }
+}
+
 int main()
 {
-    uint8_t cal = sizeof(table1)/sizeof(data1_t);
     char pub[100];
     char name[20];
-    for (uint8_t i = 0 ; i < cal; i++)
-    {
-       create_name(name,table1[i].channel, table1[i].id,table1[i].reg_adr, table1[i].title, table1[i].description);
-       createJson_provision(pub,name,table1[i].channel, table1[i].id, table1[i].type, table1[i].title, table1[i].description);
-       printf("%s\n",pub);
-    }
+    uint8_t cal = sizeof(table1)/sizeof(data1_t);
+    convert_table(table,table1);
+    // for (uint8_t i = 0; i < cal; i++)
+    // {
+    //     table[i].channel = table1[i].channel;
+    //     table[i].id = table1[i].id;
+    //     table[i].func = table1[i].func;
+    //     table[i].reg_adr = table1[i].reg_adr;
+    //     create_name(name,table1[i].channel, table1[i].id,table1[i].reg_adr, table1[i].title, table1[i].description);
+    //     table[i].alias = name;
+    //     printf("%d\t%d\t%d\t%d\t%s\n", table[i].channel, table[i].id, table[i].func, table[i].reg_adr,  table[i].alias);
+    // }
 }
