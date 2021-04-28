06b02acd91aa175e6977b4a1fb176471cd63a88d
Tram Tran <trantram2299@gmail.com>
2021-04-29 01:01:19 +0700

    add

--- Thesis/provision.c
+++ Thesis/provision.c
@@ -20,6 +20,10 @@ data1_t table1[] =
 		0,	2,	3,	4,	 "METER",	 	"CURRENT",	    "NUMBER",
 		0,	2,	3,	5,	 "METER",		"VOLTAGE",	    "NUMBER",
 		0,	2,  3, 	6,	 "INVERTER",	"POWER",	    "NUMBER",
+        1,  3,  6,  10,   "RELAY",       "LIGHT",       "BOOLEAN",
+        1,  3,  6,  15,   "RELAY",       "LIGHT",       "BOOLEAN",
+        2,  4,  6,  11,   "RELAY",       "FAN",       "BOOLEAN",
+       
 };
 uint8_t  createJson_provision(char *buffer, char *name,uint8_t channel, uint8_t id, char *type, char *title, char *description)
 {
@@ -42,7 +46,7 @@ uint8_t  createJson_provision(char *buffer, char *name,uint8_t channel, uint8_t 
         strcat(buffer,"\"");
         strcat(buffer,"}");
 }
-uint8_t create_name(char buffer[20],uint8_t channel,uint8_t id, char *title, char *description)
+uint8_t create_name(char buffer[20],uint8_t channel,uint8_t id,uint16_t reg, char *title, char *description)
 {
     memset(buffer,0,20);
     char temp[5];
@@ -59,11 +63,45 @@ uint8_t create_name(char buffer[20],uint8_t channel,uint8_t id, char *title, cha
             strcat(buffer,"_");
             strncpy(temp,title,4);
             strcat(buffer,temp);
+             memset(temp,0,4);
+            sprintf(temp,"%d",reg);
+            strcat(buffer,temp);
+            break;
+         }
+         case 1:
+        {
+            strcat(buffer,"CH1_");
+            strncpy(temp,description,3);
+            strcat(buffer,temp);
+            memset(temp,0,4);
+            sprintf(temp,"%d",id);
+            strcat(buffer,temp);
+            strcat(buffer,"_");
+            strncpy(temp,title,4);
+            strcat(buffer,temp);
+             memset(temp,0,4);
+            sprintf(temp,"%d",reg);
+            strcat(buffer,temp);
+            break;
+         }
+        case 2:
+        {
+            strcat(buffer,"CH2_");
+            strncpy(temp,description,3);
+            strcat(buffer,temp);
+            memset(temp,0,4);
+            sprintf(temp,"%d",id);
+            strcat(buffer,temp);
+            strcat(buffer,"_");
+            strncpy(temp,title,4);
+            strcat(buffer,temp);
+             memset(temp,0,4);
+            sprintf(temp,"%d",reg);
+            strcat(buffer,temp);
             break;
          }
     }
 }
-
 int main()
 {
     uint8_t cal = sizeof(table1)/sizeof(data1_t);
@@ -73,7 +111,7 @@ int main()
     
     for (uint8_t i = 0 ; i < cal; i++)
     {
-       create_name(name,table1[i].channel, table1[i].id, table1[i].title, table1[i].description);
+       create_name(name,table1[i].channel, table1[i].id,table1[i].reg_adr, table1[i].title, table1[i].description);
        createJson_provision(pub,name,table1[i].channel, table1[i].id, table1[i].type, table1[i].title, table1[i].description);
        printf("%s\n",pub);
     }
