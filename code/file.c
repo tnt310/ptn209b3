b928d54163f4db561a07d8cce474b981f742c5a0
Tram Tran <trantram2299@gmail.com>
2021-06-08 02:23:32 +0700

    add

--- code/file.c
+++ code/file.c
@@ -17,7 +17,7 @@ static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
 	return -1;
 }
 /*------------------------------------------------------------------------------------------------------------------------------*/
-data1_t *parse(char *Buffer, uint16_t BufferLen)
+data1_t *parse_device(char *Buffer, uint16_t BufferLen)
 {
     #define JSON_MAX_LEN 200
     int i,r;
@@ -120,10 +120,15 @@ void AddDevice(data1_t *destination, data1_t *data)
     //         destination->func,destination->deviceChannel,destination->deviceType, destination->deviceName,
     //         destination->channeltitle,destination->valueType, destination->devicestatus);
 }
+void assign(char *destination, char *source)
+{
+    destination = malloc(strlen(source)+1);
+    strcpy(destination, source);
+}
 void LoadDevice(void)
 {
-    static data1_t *p;
     static data1_t *dynamic;
+    static data1_t *p, temp;
     FILE *file;
     char line[200];
     static uint8_t k = 0;
@@ -138,41 +143,51 @@ void LoadDevice(void)
     if (dynamic!=NULL){
         file = fopen("DEVICE.TXT","r");
         while(fgets(line,200,file)){
-            p = parse((char*)line, strlen(line));
+            p = parse_device((char*)line, strlen(line));
             //AddDevice((dynamic+j),p);
-            (dynamic+j)->channel = (*p).channel;
-            (dynamic+j)->deviceID = (*p).deviceID;
-            (dynamic+j)->func = (*p).func;
-            (dynamic+j)->devicestatus = (*p).devicestatus;
-            (dynamic+j)->deviceChannel = (*p).deviceChannel;
-            (dynamic+j)->deviceType = (*p).deviceType;
-            (dynamic+j)->deviceName = (*p).deviceName;
-            (dynamic+j)->channeltitle = (*p).channeltitle;
-            (dynamic+j)->valueType = (*p).valueType;
+            (dynamic+j)->channel = p->channel;
+            (dynamic+j)->deviceID = p->deviceID;
+            (dynamic+j)->func = p->func;
+            (dynamic+j)->devicestatus = p->devicestatus;
+            (dynamic+j)->deviceChannel = p->deviceChannel;
+            (dynamic+j)->deviceType = p->deviceType;
+            (dynamic+j)->deviceName = p->deviceName;
+            (dynamic+j)->channeltitle = p->channeltitle;
+            (dynamic+j)->valueType = p->valueType;
+            // assign((dynamic+j)->deviceType ,(p->deviceType));
+            // assign((dynamic+j)->deviceName,(p->deviceName));
+            // assign((dynamic+j)->channeltitle ,(p->channeltitle));
+            // assign((dynamic+j)->valueType ,(p->valueType));
+            // (dynamic+j)->deviceType = "INVERTER";
+            // (dynamic+j)->deviceName = "INVERTER_01";
+            // (dynamic+j)->channeltitle = "VOLATGE";
+            // (dynamic+j)->valueType = "NUMBER";
+
+
             // printf("\nline %d: %d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d",j,
             //         &(dynamic+j)->channel,&(dynamic+j)->deviceID,&(dynamic+j)->func,&(dynamic+j)->devicestatus,
             //         &(dynamic+j)->deviceChannel,&(dynamic+j)->deviceType,&(dynamic+j)->deviceName,
             //         &(dynamic+j)->channeltitle,&(dynamic+j)->valueType);
-            // printf("\nLine %d: %d\t%d\t%d\t%d\t%d\t%s\t%s\t%s\t%s",j,
-            //         (dynamic+j)->channel,(dynamic+j)->deviceID,(dynamic+j)->func,(dynamic+j)->devicestatus,
-            //         (dynamic+j)->deviceChannel,(dynamic+j)->deviceType,(dynamic+j)->deviceName,
-            //         (dynamic+j)->channeltitle,(dynamic+j)->valueType);
             printf("\nLine %d: %d\t%d\t%d\t%d\t%d\t%s\t%s\t%s\t%s",j,
-                    p->channel,p->deviceID,p->func,p->devicestatus,
-                    p->deviceChannel,p->deviceType,p->deviceName,p->channeltitle,p->valueType);
+                    (dynamic+j)->channel,(dynamic+j)->deviceID,(dynamic+j)->func,(dynamic+j)->devicestatus,
+                    (dynamic+j)->deviceChannel,(dynamic+j)->deviceType,(dynamic+j)->deviceName,
+                    (dynamic+j)->channeltitle,(dynamic+j)->valueType);
+            // printf("\nLine %d: %d\t%d\t%d\t%d\t%d\t%s\t%s\t%s\t%s",j,
+            //         p->channel,p->deviceID,p->func,p->devicestatus,
+            //         p->deviceChannel,p->deviceType,p->deviceName,p->channeltitle,p->valueType);
             j++;
         }
         fclose(file);
     }
     printf("\n\n----------------DEVICES ADDRESS WERE ADDED TO DYNAMIC ARRAY--------------------------------");
-        for (uint8_t i = 0; i < k; i++){
-        printf("\nLine %d: %d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d",i,
-                &(dynamic+i)->channel,&(dynamic+i)->deviceID,&(dynamic+i)->func,&(dynamic+i)->devicestatus,
-                &(dynamic+i)->deviceChannel,&(dynamic+i)->deviceType,&(dynamic+i)->deviceName,
-                &(dynamic+i)->channeltitle,&(dynamic+i)->valueType);
-    }
+    //     for (uint8_t i = 0; i < k; i++){
+    //     printf("\nLine %d: %d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d",i,
+    //             &(dynamic+i)->channel,&(dynamic+i)->deviceID,&(dynamic+i)->func,&(dynamic+i)->devicestatus,
+    //             &(dynamic+i)->deviceChannel,&(dynamic+i)->deviceType,&(dynamic+i)->deviceName,
+    //             &(dynamic+i)->channeltitle,&(dynamic+i)->valueType);
+    // }
     printf("\n\n----------------DEVICES VALUE WERE ADDED TO DYNAMIC ARRAY--------------------------------");
-        for (uint8_t i = 0; i < k; i++){
+        for (uint8_t i = 1; i < k; i++){
             printf("\nLine %d: %d\t%d\t%d\t%d\t%d\t%s\t%s\t%s\t%s",i,
                     (dynamic+i)->channel,(dynamic+i)->deviceID,(dynamic+i)->func,(dynamic+i)->devicestatus,
                     (dynamic+i)->deviceChannel,(dynamic+i)->deviceType,(dynamic+i)->deviceName,
