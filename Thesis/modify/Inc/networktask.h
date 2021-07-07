8c9950fc658aa8fb96b830dc25e66579492a3151
Tram Tran <trantram2299@gmail.com>
2021-07-07 13:20:07 +0700

    add

--- Thesis/modify/Inc/networktask.h
+++
@@ -1,8 +0,0 @@
-#ifndef _NETWORKTASK__H_
-#define _NETWORKTASK__H_
-
-#define HTTP_CMD_GET		"GET "
-#define HTTP_CMD_POST		"POST "
-
-
-#endif
--- Thesis/modify/Middlewares/Third_Party/modbus/mb_app_m.h
+++
@@ -1,4 +0,0 @@
-#ifndef _MB__APP__M_H__
-#define _MB__APP__M_H__
-
-#endif
