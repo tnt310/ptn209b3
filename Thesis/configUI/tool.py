5d9c35912d127d3a00bdc59021190eb33124be25
Tram Tran <trantram2299@gmail.com>
2021-07-15 02:36:09 +0700

    add

--- Thesis/configUI/tool.py
+++ Thesis/configUI/tool.py
@@ -30,41 +30,44 @@ class Gateway(QMainWindow):
         self.gateway = Ui_gateway()
         self.serial = Serial()
         if self.serial:
-            self.gateway.setupUi(self)
             self.gateway_setting()
         else:
             print("NOT OPEN")
 
     def gateway_setting(self):
+        self.gateway.setupUi(self)
         self.gateway.gatewaysetting.clicked.connect(self.gateway_setting)
         self.gateway.devicesetting.clicked.connect(self.device_setting)
         self.gateway.networksetting.clicked.connect(self.network_setting)
         self.gateway.telemetrycontrol.clicked.connect(self.telemetry_control)
+        self.gateway.testtool.clicked.connect(self.test_tool)
         self.gateway.updateport.clicked.connect(self.update_rs485)
         self.gateway.updatemqtt.clicked.connect(self.update_mqtt)
         self.gateway.updatetimeout.clicked.connect(self.update_timeout)
         self.gateway.updateapikey.clicked.connect(self.update_apikey)
+
     def update_rs485(self):
-        baudrate = self.gateway.baudrate.currentData()
-        if self.gateway.databit.currentData() == 8:
-            databit = 0
-        if self.gateway.stopbit.currentData() == 1:
-            stopbit = 0
-        if self.gateway.parity.currentText() == "NONE":
-            parity = 0
-        # str = self.gateway.port.currentText()+' '+ self.gateway.baudrate.currentText()+' '+ self.gateway.databit.currentText()+' '+ self.gateway.stopbit.currentText()+' '+ self.gateway.parity.currentText()
-        string = str(baudrate)+' '+str(databit)+' '+str(stopbit)+' '+str(parity)
-        self.serial.send(string)
+        global baud, databit, stopbit, parity
+        baud = self.gateway.baudrate.currentData()
+        # if self.gateway.databit.currentData() == 8:
+        #     databit = '0'
+        # if self.gateway.stopbit.currentData() == 1:
+        #     stopbit = '0'
+        # if self.gateway.parity.currentText() == "NONE":
+        #     parity = '0'
+        print(self.gateway.databit.currentData())
+        # string = str(baud)+ databit + stopbit+ parity
+        # self.serial.send(string)
     def update_mqtt(self):
-        str = self.gateway.broker.text()+' '+ self.gateway.username.text()+' '+ self.gateway.password.text()+' '+ self.gateway.mqttport.text()
+        str = 'configmqtt'+' '+self.gateway.broker.text()+' '+ self.gateway.username.text()+' '+ self.gateway.password.text()+' '+ self.gateway.mqttport.text()+'\r'
         self.serial.send(str)
-
     def update_timeout(self):
-        self.serial.send(self.gateway.timeout.currentText())
-
+        temp = self.gateway.timeout.currentText().split()
+        str = 'configtimeout'+' '+ temp[0]+'\r'
+        self.serial.send(str)
     def update_apikey(self):
-        print("SEND READY")
-        self.serial.send(self.gateway.apikey.text())
+        str = 'configapikey'+' '+ self.gateway.apikey.text()+'\r'
+        self.serial.send(str)
 
     def device_setting(self):
         self.device = Ui_device()
@@ -76,6 +79,12 @@ class Gateway(QMainWindow):
         self.device.testtool.clicked.connect(self.test_tool)
         self.device.add.clicked.connect(self.addDevice)
 
+    def addDevice(self):
+        self.dialog = Dialog()
+        if self.dialog.exec_():
+            self.type = dialog.save()
+            print(self.type)
+
     def network_setting(self):
         self.network = Ui_network()
         self.network.setupUi(self)
@@ -84,6 +93,10 @@ class Gateway(QMainWindow):
         self.network.networksetting.clicked.connect(self.network_setting)
         self.network.telemetrycontrol.clicked.connect(self.telemetry_control)
         self.network.testtool.clicked.connect(self.test_tool)
+        self.network.update.clicked.connect(self.update_network)
+    def update_network(self):
+        str = 'confignetwork'+' '+self.network.ip.text()+' '+ self.network.netmask.text()+' '+ self.network.gateway.text()+'\r'
+        self.serial.send(str)
 
     def telemetry_control(self):
         self.control = Ui_telemetry()
@@ -93,6 +106,18 @@ class Gateway(QMainWindow):
         self.control.networksetting.clicked.connect(self.network_setting)
         self.control.telemetrycontrol.clicked.connect(self.telemetry_control)
         self.control.testtool.clicked.connect(self.test_tool)
+        self.control.provision.clicked.connect(self.send_provision)
+        self.control.telemetryon.clicked.connect(self.set_telemetry)
+        self.control.telemetryoff.clicked.connect(self.off_telemetry)
+    def send_provision(self):
+        str = 'sendprovision\r'
+        self.serial.send(str)
+    def set_telemetry(self):
+        str = 'configtelemetry 1\r'
+        self.serial.send(str)
+    def off_telemetry(self):
+        str = 'configtelemetry 0\r'
+        self.serial.send(str)
 
     def test_tool(self):
         self.test = Ui_toolLogin()
@@ -103,11 +128,6 @@ class Gateway(QMainWindow):
         self.test.telemetrycontrol.clicked.connect(self.telemetry_control)
         self.test.testtool.clicked.connect(self.test_tool)
 
-    def addDevice(self):
-        self.dialog = Dialog()
-        if self.dialog.exec_():
-            self.type = dialog.save()
-            print(self.type)
 
 
 
